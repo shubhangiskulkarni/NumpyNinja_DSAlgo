08900b1ad47fb114b8cc9f3935e92aaf25648fc9
Shubhangi Kulkarni <shubhangiskulkarni@gmail.com>
2023-02-21 20:37:47 -0600

    Changes for SignInPage to read data from ExcelUtils

--- src/test/java/com/DSAlgo/qa/testcase/SignInPageTest.java
+++ src/test/java/com/DSAlgo/qa/testcase/SignInPageTest.java
@@ -4,6 +4,7 @@ import com.DSAlgo.qa.base.TestBase;
 import com.DSAlgo.qa.pages.GetStartedPage;
 import com.DSAlgo.qa.pages.HomePage;
 import com.DSAlgo.qa.pages.SignInPage;
+import com.DSAlgo.qa.util.ExcelUtils;
 import com.DSAlgo.qa.util.Loggerload;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -49,12 +50,15 @@ public class SignInPageTest extends TestBase {
 
     //Test Case #24
     @Test(priority = 2)
-    public void validateUserLoggedInMsg() {
+    public void validateUserLoggedInMsg() throws Exception {
 
         Loggerload.info("Executing test case to validate login message.");
 
-        signInPage.enterUserName("shubhangi");
-        signInPage.enterPwd("DSAlgoA2S2");
+        Object[][] testObjArray = ExcelUtils.getTableArray(prop.getProperty("TestDataSheet"), "SignIn");
+
+        signInPage.enterUserName(testObjArray[0][1].toString());
+        signInPage.enterPwd(testObjArray[0][2].toString());
+
         signInPage.clickOnLoginBtn();
         String alertMsg = signInPage.checkAlertMsg();
         Assert.assertEquals(alertMsg, "You are logged in", "Checking for valid alert message.");
@@ -62,10 +66,14 @@ public class SignInPageTest extends TestBase {
     }
 
     @Test(priority = 3)
-    public void validateUserNmDisplayed() {
+    public void validateUserNmDisplayed() throws Exception  {
+
         Loggerload.info("Executing test case to validate user name display.");
-        signInPage.enterUserName("shubhangi");
-        signInPage.enterPwd("DSAlgoA2S2");
+
+        Object[][] testObjArray = ExcelUtils.getTableArray(prop.getProperty("TestDataSheet"), "SignIn");
+        signInPage.enterUserName(testObjArray[0][1].toString());
+        signInPage.enterPwd(testObjArray[0][2].toString());
+
         signInPage.clickOnLoginBtn();
         String userNmDisplay = signInPage.isLoginNmDisplayed();
         Assert.assertEquals(userNmDisplay, "Shubhangi");
